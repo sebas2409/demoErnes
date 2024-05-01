922ade8dfc131b6f7a985c9903c03958ddbc310f
sebas2409 <juangonzalezlopez241829@gmail.com>
2024-05-01 20:31:08 +0200

    formateo de código

--- playground/views.py
+++ playground/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
 
+
 def ping(request):
     return HttpResponse('Pong!')
 
@@ -9,7 +10,7 @@ def ping(request):
 def index(request):
     return render(request, 'index.html')
 
+
 def date(request):
     ahora = datetime.now().strftime('%b %dth, %Y -%H:%M hrs')
     return HttpResponse(f"Hi, this is a greeting from Django, at {ahora}")
-    
